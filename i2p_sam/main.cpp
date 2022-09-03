eeaa08296a109c4ae1d5838c0fe24f9533ff38ee
bigBonSlime <true_big_bon_slime@rambler.ru>
2022-09-03 23:46:17 +0300

    fix format

--- i2p_sam/main.cpp
+++ i2p_sam/main.cpp
@@ -3,25 +3,4 @@
 #include <iostream>
 #include <string>
 
-int counter = 0;
-
-void readdata(std::shared_ptr<i2p_sam::datagram_session> sock) {
-    sock->async_read_datagram(
-        [sock]([[maybe_unused]] std::string dest, [[maybe_unused]] std::size_t size,
-               [[maybe_unused]] uint16_t from_port, [[maybe_unused]] uint16_t to_port,
-               std::shared_ptr<std::byte[]> data, i2p_sam::errors::sam_error ec) {
-            if (!ec) {
-                std::string s((char *)data.get(), size);
-                std::cout << s << " " << counter << '\n';
-                counter += 1;
-                readdata(sock);
-            } else {
-
-                std::cout << ec.what();
-            }
-        });
-}
-
-int main() {
-
-}
+int main() {}
